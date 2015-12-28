adea7d9eae529618b797547df78face8577e8fa6
YD <YD@YD.local>
2015-12-28 10:25:12 +0800

    not connect to next page

--- manager_js.js
+++ manager_js.js
@@ -38,3 +38,16 @@ function tranSub(){
     document.getElementsByName("d[" + counterArray[i] + "]")[0].remove();
   }
 }
+window.onload=function(){
+  var password = document.getElementById("password")
+    , confirm_password = document.getElementById("confirm_password");
+  function validatePassword(){
+    if(password.value != confirm_password.value) {
+      confirm_password.setCustomValidity("Passwords Don't Match");
+    } else {
+      confirm_password.setCustomValidity('');
+    }
+  }
+  password.onchange = validatePassword;
+  confirm_password.onkeyup = validatePassword;
+}
